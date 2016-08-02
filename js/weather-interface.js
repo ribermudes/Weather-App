d33a6c17e31f4d9b21d5ca4c0fd921141e81368f
Noah Yasskin and Rachael Bermudes <rachael@>
2016-08-02 16:45:12 -0700

    add javascript files

---
+++ js/weather-interface.js
@@ -0,0 +1,14 @@
+var apiKey = require('./../.env').apiKey;
+var Weather = require('./../js/weather.js').weatherModule;
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    var city = $('#location').val();
+    $('#location').val("");
+    $.get('http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey).then(function(response) {
+      $('.showWeather').text("The humidity in " + city + " is " + response.main.humidity + "%");
+    }).fail(function(error) {
+      $('.showWeather').text(error.responseJSON.message);
+    });
+  });
+});
---
+++ js/weather.js
@@ -0,0 +1,8 @@
+function Weather(){
+}
+
+Weather.prototype.getWeather = function() {
+  console.log("Hi! I'm a weather object.");
+}
+
+exports.weatherModule = Weather;
